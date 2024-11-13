637e387b4491ed151959510de773a1e208458b87
sofaspawn <h.m1nus.work@gmail.com>
2024-11-13 10:45:08 +0530

    handling

--- day1.py
+++ day1.py
@@ -1,10 +1,12 @@
 #! /usr/bin/env python3
-
 import string
+import re
 
-f = open("input1.txt", "r")
+f = open("input1.txt", "r", encoding="utf-8")
 lines = [l.strip() for l in f.readlines()]
 
+#part 1
+'''
 sum = 0
 
 for line in lines:
@@ -13,7 +15,14 @@ for line in lines:
     num = fno+lno
     num = int(num)
     sum+=num
-
 #print(sum)
-
+'''
 #part 2
+numbers_as_words = r"\b(?:one|two|three|four|five|six|seven|eight|nine)\b"
+#numbers_as_digits = r"\b\d+\b"
+pattern = re.compile(numbers_as_words, re.IGNORECASE)
+for line in lines:
+    matches = pattern.findall(line)
+    if matches:
+        print("line",line)
+        print("matches",matches)
