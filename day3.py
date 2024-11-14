33d65df53414fd2e4f2e3b0f9ae66263abdb8fbc
sofaspawn <h.m1nus.work@gmail.com>
2024-11-14 13:38:19 +0530

    making progress

--- day3.py
+++ day3.py
@@ -3,6 +3,25 @@
 with open('input3.txt') as f:
     data = f.read()
 
-for line in data.strip().split('\n'):
-    print(line)
+datarr = [line for line in data.strip().split('\n')]
+
+def getnumber(datarr, y, x):
+    s = ""
+    i = 0
+    while datarr[y][x+i].isdigit():
+        s+=datarr[y][x+i]
+        i+=1
+    return int(s),i
+
+y=0
+x=0
+
+while y<len(datarr):
+    while x<len(datarr[y]):
+        if datarr[y][x].isdigit():
+            number,i = getnumber(datarr, y, x)
+            x+=i
+            print(number)
+        x+=1
+    y+=1
     break
